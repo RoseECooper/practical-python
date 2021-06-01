948821f178530932de7118d4f4b394a2e0e62e1c
RoseECooper <rose.cooper@stfc.ac.uk>
2021-06-01 16:57:26 +0100

    simple beginner exercises

---
+++ Work/sample_code.py
@@ -0,0 +1,16 @@
+#stack dollar bills to be the height of the sears building
+bill_thickness=0.11*0.001
+sears_height=442
+num_bills=1
+day=1
+
+#while loop to calc. stack height
+while num_bills*bill_thickness<sears_height: 
+    print(day, num_bills, num_bills*bill_thickness)
+    day=day+1
+    num_bills=num_bills*2
+
+print('Number of days', day)
+print('Number of bills', num_bills)
+print('Final height', num_bills*bill_thickness)
+
