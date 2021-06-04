1683e9db98b83b8251cbf50c9e777c3b8a1aafae
RoseECooper <rose.cooper@stfc.ac.uk>
2021-06-04 11:59:25 +0100

    Completed section 1 exercises

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,9 +5,26 @@ principal=500000.0
 rate=0.05
 payment=2684.11
 total_paid=0.0
+month=0
+
+extra_payment_start_month=61
+extra_payment_end_month=108
+extra_payment=1000
 
 while principal>0:
+    month=month+1
     principal=principal*(1+rate/12)-payment
     total_paid=total_paid+payment
 
-print('Total paid', total_paid)
+    if month>=extra_payment_start_month and month<=extra_payment_end_month:
+        principal=principal-extra_payment
+        total_paid=total_paid+extra_payment
+
+    if 0<principal<payment:
+        final_payment=payment-principal
+        total_paid=total_paid+final_payment
+
+    #print('Months repaying:', month, 'Principal amount remaining:', round(principal,2), 'Total paid to date:', round(total_paid,2))
+    print(f'{round(total_paid,2):<10} | {round(principal,2):<10} | {month:<10}')
+
+print(f'Paid a total of {round(total_paid,2)} over {month} months')
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,32 @@
 # pcost.py
 #
 # Exercise 1.27
+
+import csv
+import sys 
+
+def portfolio_cost(filename):
+
+    total_cost=0.0
+
+    with open (filename,'rt') as csv_file: 
+        rows=csv.reader(csv_file)
+        headers=next(rows) 
+        for row in rows:
+            try:
+                no_shares=int(row[1])
+                price=float(row[2])
+                total_cost+=no_shares*price
+            except ValueError:
+                print('Bad data', row)
+        print('Total portfolio cost is: ',total_cost) 
+
+    return total_cost 
+
+if len(sys.argv)==2:
+    filename=sys.argv[1]
+else:
+    filename=input('Enter a filename:')
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
